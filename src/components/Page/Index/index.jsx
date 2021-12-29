0ab395062bc45fc25a1ab19c533a324eb3d45aa3
MinusFour <alex@minusfour.net>
2021-12-29 14:59:04 -0600

    Add services grid and further adjustements to logo
    Some of the SVGs need further work and the grid is still incomplete, the
    font is adjusted so it uses paths instead of the font (so the user
    doesn't need the font).

--- src/components/Page/Index/index.jsx
+++ src/components/Page/Index/index.jsx
@@ -3,6 +3,7 @@ import Header from '@Components/Layout/Header';
 import News from '@Components/Layout/News';
 import ForumStats from '@Components/Layout/Forum';
 import Footer from '@Components/Layout/Footer';
+import Services from '@Components/Layout/Services';
 import {
     links,
     announcements,
@@ -13,12 +14,14 @@ import {
     onlineUsers,
     topTopics,
     footerLinks,
+    services,
 } from '@Util/data.js';
 
 const App = () => (
     <>
         <Header links={links} />
         <News announcements={announcements} articles={articles} />
+        <Services sections={services} />
         <ForumStats
             generalStatistics={generalStatistics}
             topWeeklyPosters={topWeeklyPosters}
